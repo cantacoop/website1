6a5c888fe29a871c54aa68fe6fe1540394a0d7b3
Prawit Boonthue <prawit_bo@dtgo.com>
2022-03-02 10:12:09 +0700

    Connect to webproxy

--- html/index.php
+++ html/index.php
@@ -6,5 +6,6 @@
     $dbhandle = mysqli_connect($servername, $username, $password);
     $selected = mysqli_select_db($dbhandle, "titanic");
 
+    echo "Hello : from www.porterverse.com<br>";
     echo "Connected database server<br>";
     echo "Selected database";
