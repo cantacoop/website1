dfb70632530bcf2fdb5f1dfb5b2ceadf4a461e2b
Prawit Boonthue <prawit_bo@dtgo.com>
2022-03-02 09:09:16 +0700

    Initial project

---
+++ html/index.php
@@ -0,0 +1,10 @@
+<?php
+    $servername = "lemp_mariadb";
+    $username = "devops";
+    $password = "devops101";
+
+    $dbhandle = mysqli_connect($servername, $username, $password);
+    $selected = mysqli_select_db($dbhandle, "titanic");
+
+    echo "Connected database server<br>";
+    echo "Selected database";
